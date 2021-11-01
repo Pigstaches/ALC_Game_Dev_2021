767c76380f883b2bbbac77020029cb9204ec5ab7
Ylvine <marveluniversegeek@gmail.com>
2021-11-01 13:52:23 -0600

    can Shoot hehe enemy scatter blow up

--- Prototype 2 - Top Down/FPS/Assets/Scripts/Enemy.cs
+++ Prototype 2 - Top Down/FPS/Assets/Scripts/Enemy.cs
@@ -18,27 +18,27 @@ public class Enemy : MonoBehaviour
     public float yPathOffset; 
 
     private List<Vector3> path;
-    private Weapon weapon;
+    private Weapons weapon;
     private GameObject target; 
 
     // Start is called before the first frame update
     void Start()
     {
         //Gather the Components
-        weapon = GetComponents<Weapon>();
+        weapon = GetComponent<Weapons>();
         target = FindObjectOfType<PlayerController>().gameObject;
 
         InvokeRepeating("UpdatePath", 0.0f, 0.5f);
     }
 
-    void UpdatePath ()
+    void UpdatePath()
     {
         //Calculate a path to the target
         NavMeshPath navMeshPath = new NavMeshPath();
         NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, navMeshPath);
 
         //Save path as a list 
-        path = navMesh.corners.ToList();
+        path = navMeshPath.corners.ToList();
     }
 
 
@@ -47,16 +47,46 @@ public class Enemy : MonoBehaviour
         if(path.Count == 0)
             return;
         //move towards the closest pat
-        transform.position = Vector3.MoveTowards(transform.position, path[0] + new Vector3(0, yPathOffset, 0), moeSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, path[0] + new Vector3(0, yPathOffset, 0), moveSpeed * Time.deltaTime);
         
-        if(transform.postion == path[0] + new Vector3(0, yPathOffset, 0))
+        if(transform.position == path[0] + new Vector3(0, yPathOffset, 0))
             path.RemoveAt(0);
     }
 
 
+    public void TakeDamage(int damage)
+    {
+        curHP -= damage;
+
+        if(curHP <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+       Destroy(gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // look at target
+        Vector3 dir = (target.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
         
+        transform.eulerAngles = Vector3.up * angle;
+        
+        // get distance from enemy to player/target
+        float dist = Vector3.Distance(transform.position, target.transform.position);
+
+        if(dist <= attackRange)
+        {
+            if (weapon.CanShoot());
+                weapon.Shoot();
+        }
+        else
+        {
+            ChaseTarget();
+        }
     }
 }
