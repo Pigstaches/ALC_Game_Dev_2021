e2c654101071c38334ef3b44cdc13ad0df5b36c8
Ylvine <marveluniversegeek@gmail.com>
2021-12-08 15:10:01 -0700

    materials and script fix...
    Walking through walls
    Particle + disappear only on walls, not even floor
    enemy don't die
    UI won't show in-game

--- Prototype 2 - Top Down/FPS/Assets/Scripts/GameUi.cs
+++ Prototype 2 - Top Down/FPS/Assets/Scripts/GameUi.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class GameUi : MonoBehaviour
+public class GameUI : MonoBehaviour
 {
     [Header("Hud")]
     public TextMeshProUGUI scoreText;
